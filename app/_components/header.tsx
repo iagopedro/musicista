758f533d8e7a69c1e346a399e9b469cb5cefdbb4
iagopedro <iagopedroltd19@gmail.com>
2024-06-16 11:10:34 -0300

    chore: add shadcn init setup and feat: add initial header component

---
+++ app/_components/header.tsx
@@ -0,0 +1,21 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
+export default function Header() {
+  return (
+    <header>
+      <Card>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+      </Card>
+    </header>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import Header from "./_components/header";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold">Hello, Next.js!</h1>;
+  return (
+    <Header />
+  );
 }
